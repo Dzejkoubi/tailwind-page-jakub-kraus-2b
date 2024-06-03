924419dc9b5d035480e276617a993373738cc7f8
Dzejkoubi <kubakraus06@gmail.com>
2024-06-03 23:49:28 +0200

    chore: Update tailwind.config.js and output.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./*.{html,js}"],
   theme: {
-    extend: {},
+    backgroundImage: {
+      "custom-gradient": "linear-gradient(to bottom right, #f5e8f7, #e2d1f5)",
+    },
   },
   plugins: [],
 };
